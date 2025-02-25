37e1c824b511941e9ea8e69eafa4dba7943235a5
Kirubel Midru <kirubelmidru@gmail.com>
2025-02-25 22:02:47 +0300

    may god help me

--- lib/twitchpage.dart
+++ lib/twitchpage.dart
@@ -1,14 +1,42 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+final TextEditingController _searchController = TextEditingController();
 class TwitchScreen extends StatelessWidget {
   const TwitchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Twitch'),
+      child: TextField(
+        controller: _searchController,
+        decoration: InputDecoration(
+          hintText: 'Search Twitch',
+          // Add a clear button to the search bar
+          prefixIcon: IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/youtube.svg',
+              color: Colors.red,
+              semanticsLabel: 'Youtube',
+              height: 100,
+              width: 70,
+            ),
+            onPressed: () {
+              // Perform the search here
+            },
+          ),
+
+          suffixIcon: IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => _searchController.clear(),
+          ),
+          // Add a search icon or button to the search bar
+
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/youtubepage.dart
+++ lib/youtubepage.dart
@@ -1,6 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+final TextEditingController _searchController = TextEditingController();
 
 class YoutubeScreen extends StatelessWidget {
   const YoutubeScreen({super.key});
@@ -8,7 +9,41 @@ class YoutubeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('YouTube'),
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Search Youtube',
+            hintStyle: TextStyle(
+              color: Color(0xFF000000),
+            ),
+            // Add a clear button to the search bar
+            prefixIcon: IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/youtube.svg',
+                color: Colors.red,
+                semanticsLabel: 'Youtube',
+              ),
+              onPressed: () {
+                // Perform the search here
+              },
+            ),
+
+            suffixIcon: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => _searchController.clear(),
+            ),
+            // Add a search icon or button to the search bar
+
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(900.0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

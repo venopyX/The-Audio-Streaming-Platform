66a76722a71491d6f1c5b1f87f343f1e8e268079
Anwar Nas <anwarnasir0970@gmail.com>
2025-03-07 17:43:32 -0900

    updated the page UI

--- lib/downloadsPage.dart
+++ lib/downloadsPage.dart
@@ -1,7 +1,7 @@
-
 import 'package:audiobinge/favoriteUtils.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:youtube_scrape_api/models/video.dart';
 import 'videoComponent.dart';
 import 'package:shimmer/shimmer.dart';
@@ -10,6 +10,7 @@ import 'package:provider/provider.dart';
 import 'downloadUtils.dart';
 import 'MyVideo.dart';
 import 'colors.dart';
+import 'connectivityProvider.dart';
 
 class DownloadScreen extends StatefulWidget {
   const DownloadScreen({super.key});
@@ -18,17 +19,34 @@ class DownloadScreen extends StatefulWidget {
   _DownloadScreenState createState() => _DownloadScreenState();
 }
 
-class _DownloadScreenState extends State<DownloadScreen> {
+class _DownloadScreenState extends State<DownloadScreen> with SingleTickerProviderStateMixin {
   List<MyVideo> _videos = [];
   bool _isLoading = false;
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
     fetchDownloads();
+    _animationController.forward();
   }
 
-  void fetchDownloads() async {
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  Future<void> fetchDownloads() async {
     setState(() {
       _isLoading = true;
     });
@@ -39,101 +57,191 @@ class _DownloadScreenState extends State<DownloadScreen> {
     });
   }
 
-
+  Future<void> _handleRefresh() async {
+    await fetchDownloads();
+    return Future.value();
+  }
 
   @override
   Widget build(BuildContext context) {
     final playing = context.watch<Playing>();
+
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Downloads',
-                  style: GoogleFonts.roboto(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
+      body: FadeTransition(
+        opacity: _animation,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+              decoration: BoxDecoration(
+                color: Colors.grey[900],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
                 ),
-                if (_videos.isNotEmpty)
-                  ElevatedButton.icon(
-                    onPressed:()=> playing.setQueue(_videos),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primaryColor, // Red color for the button
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                    ),
-                    icon: Icon(Icons.play_arrow, color: Colors.white),
-                    label: Text(
-                      'Play All',
-                      style: GoogleFonts.roboto(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(0, 3),
                   ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: _isLoading
-                ? GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 20.0,
+                ],
               ),
-              padding: EdgeInsets.all(16),
-              itemCount: 10, // Show 10 shimmer placeholders while loading
-              itemBuilder: (context, index) {
-                return Shimmer.fromColors(
-                  baseColor: Colors.grey[800]!,
-                  highlightColor: Colors.grey[700]!,
-                  child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[800],
-                      borderRadius: BorderRadius.circular(15),
-                    ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.download_rounded,
+                        color: AppColors.primaryColor,
+                        size: 28,
+                      ),
+                      SizedBox(width: 12),
+                      Text(
+                        'Downloads',
+                        style: GoogleFonts.roboto(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              },
-            )
-                : _videos.isEmpty
-                ? Center(
-              child: Text(
-                'No Downloads yet.',
-                style: TextStyle(color: Colors.white),
+                  Row(
+                    children: [
+
+                      if (_videos.isNotEmpty)
+                        SizedBox(width: 8),
+                      if (_videos.isNotEmpty)
+                        ElevatedButton.icon(
+                          onPressed: () => playing.setQueue(_videos),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primaryColor,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                          ),
+                          icon: Icon(Icons.play_arrow, size: 16),
+                          label: Text(
+                            'Play All',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ],
               ),
-            )
-                : GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 20.0,
-              ),
-              padding: EdgeInsets.all(16),
-              itemCount: _videos.length,
-              itemBuilder: (context, index) {
-                final video = _videos[index];
-                return VideoComponent(video: video);
-              },
             ),
-          ),
-        ],
+            Expanded(
+              child: LiquidPullToRefresh(
+                onRefresh: _handleRefresh,
+                color: AppColors.primaryColor, // Using app's yellowish color
+                backgroundColor: Colors.grey[900],
+                height: 100,
+                animSpeedFactor: 2,
+                showChildOpacityTransition: true,
+                child: _buildContent(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _buildContent() {
+    if (_isLoading) {
+      return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 20.0,
+          childAspectRatio: 0.7,
+        ),
+        padding: EdgeInsets.all(16),
+        itemCount: 8,
+        itemBuilder: (context, index) {
+          return Shimmer.fromColors(
+            baseColor: Colors.grey[800]!,
+            highlightColor: Colors.grey[700]!,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    if (_videos.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.download_done_rounded,
+              size: 80,
+              color: Colors.grey[700],
+            ),
+            SizedBox(height: 16),
+            Text(
+              'No downloads yet',
+              style: GoogleFonts.roboto(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[500],
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Your downloaded tracks will appear here',
+              style: TextStyle(color: Colors.grey[600]),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to YouTube page to find content to download
+                Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> YouTubeTwitchTabs()));
+
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                foregroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text('Find Tracks to Download'),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 20.0,
+        childAspectRatio: 0.7,
+      ),
+      padding: EdgeInsets.all(16),
+      itemCount: _videos.length,
+      itemBuilder: (context, index) {
+        final video = _videos[index];
+        return VideoComponent(video: video);
+      },
+    );
+  }
 }
-
-
-
